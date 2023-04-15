40b77b88e21b4e31133f1f630e2470bc564fe8c1
runTime77 <runtimeerror77@gmail.com>
2023-04-15 20:36:46 +0600

    rimi_Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Main from './Main';
+import Products from './Products';
+import ContactUs from './ContactUs';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/products",
+          element: <Products></Products>,
+        },
+        {
+          path: "/contactus",
+          element: <ContactUs></ContactUs>
+        },
+      ],
+    },
+
+
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   );
 }
 
